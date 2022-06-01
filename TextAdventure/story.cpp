d5d4b44dfcc7fe98f9d02a228b18d9373791dd6f
Kai Oestreich <51728041+Gisbert12843@users.noreply.github.com>
2022-06-01 21:09:36 +0200

    stuff

--- TextAdventure/story.cpp
+++ TextAdventure/story.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
 #include "story.h"
+#define text(a); std::cout << a << std::endl;
 
 bool greenforest()
 {
+	system("cls");
+	text("You see nothing but white when you first wake up.")
 
 
 
-
-
+	return true;
 }
